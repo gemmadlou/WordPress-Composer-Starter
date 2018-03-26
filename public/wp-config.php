f8f3b95661515737a5327f12904f69b0daae2829
Gemma Black <gemma.black@breakthroughmedia.org>
2018-03-26 14:51:48 +0100

    Environment File Driven Configuration

--- public/wp-config.php
+++ public/wp-config.php
@@ -3,26 +3,32 @@
  * WordPress Config file
  */
 
+// Include Composer Dependencies
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load the DotEnv Package to access settings with the .env environment file
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
 
 /**
  * Database name
  */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', getenv('DATABASE_NAME'));
 
 /**
  * Database user
  */
-define('DB_USER', 'root');
+define('DB_USER', getenv('DATABASE_USER'));
 
 /**
  * Database user password
  */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', getenv('DATABASE_PASSWORD'));
 
 /**
  * Database host
  */
-define('DB_HOST', 'localhost');
+define('DB_HOST', getenv('DATABASE_HOST'));
 
 /**
  * WordPress DB Charset (is setup this way when the tables are made)
@@ -58,26 +64,26 @@ define('WP_PLUGIN_DIR', dirname(__FILE__) . '/wp-content/plugins');
  * Controls the error reporting. When true, it sets the error reporting level
  * to E_ALL. 
  */
-define( 'WP_DEBUG', true );
+define('WP_DEBUG', filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN));
 
 /**
  * If error logging is enabled, this determines whether the error
  * is logged or not in the debug.log file inside /wp-content.
  */
-define( 'WP_DEBUG_LOG', true );
+define('WP_DEBUG_LOG', filter_var(getenv('DEBUG_LOG'), FILTER_VALIDATE_BOOLEAN));
 
 /**
  * If error logging is enabled, this determines whether the error is
  * shown on the site (in-browser)
  */
-define( 'WP_DEBUG_DISPLAY', true );
+define('WP_DEBUG_DISPLAY', filter_var(getenv('DEBUG_DISPLAY'), FILTER_VALIDATE_BOOLEAN));
 
 /**
  * This disables live edits of theme and plugin files on the WordPress
  * administration area. It also prevents users from adding, 
  * updating and deleting themes and plugins.
  */
-define( 'DISALLOW_FILE_MODS', true );
+define('DISALLOW_FILE_MODS', true);
 
 /**
  * Prevents WordPress core updates, as this is controlled through
@@ -90,17 +96,16 @@ define( 'WP_AUTO_UPDATE_CORE', false );
  */
 $table_prefix = 'wp_';
 
-
 /* Authentication Unique Keys and Salts. */
 /* https://api.wordpress.org/secret-key/1.1/salt/ */
-define('AUTH_KEY',         'wIn%@e9$=Bd~|XhJmBwN@9O@h[XdfxrUB%nlZ+-:YBzX!]9F2yfa`u]<EBo(zpI+');
-define('SECURE_AUTH_KEY',  'X#H:7!A[6Z-L~X>>*edJ;lfSbbN`ac?N-OpXD.Xo8)n/(`<;d&~q Q?naQ=W_9Q*');
-define('LOGGED_IN_KEY',    'y.$s|J%|js$2VWMK7I@wmvLVr8G6ZSF{xX2&6j`^7Os^PF1?>bGN$Ek<Hk!ds5jQ');
-define('NONCE_KEY',        'X4RB*VCRp|`CdY|X=q7gl0(WBtH9ZT|#C#+j[0/c|I[Cyv[>gU+HYv]rzFWr{A6 ');
-define('AUTH_SALT',        '!$PLxI_QBo-+UY3fz<YajT4v{/45+TRz]h#)*;0578w;)S9K:vE0jaR0eEZY5A96');
-define('SECURE_AUTH_SALT', 'pHrah=@R9!Z@%{)@99Kh=Do5FBU}AO-wsr:5e[W7=^#MFfPs[18:v6XGNl+|;WB;');
-define('LOGGED_IN_SALT',   '3AbG|77,)zo6>K_|0|O3@-,FlK*,+|bVXi~(`bYH?a5D~4kYB6UA)M<l+Z/7-gS@');
-define('NONCE_SALT',       'x_=.6xZ>%[lu?aK $-@i}CvYDAm%0,GD:Ml!cH6K+ZQgr)<77L6D>Pc_G88+@=uU');
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
 
 
 /* Absolute path to the WordPress directory. */
